63719c80ca168344992ab3b790b8576a601fffc6
Louis Neighbours <lou.neighbours@xtra.co.nz>
2020-11-03 11:18:50 +1300

    Update site

--- assets/js/default.js
+++ assets/js/default.js
@@ -2,34 +2,23 @@ $(function() {
   var body = document.body;
   var $body = $(body);
   var video = document.getElementById('imvideo');
-  var playing = false;
 
-  video.addEventListener('loadeddata', function () {
-    body.classList.remove('imvideoloading');
-
-    function playMedia(event) {
-      if ($(event.target).hasClass('imblockvidctrl')) {
-        return;
-      };
-      if (playing === false) {
-        body.classList.add('implayed');
-        playing = true;
-      }
-      if (video.paused) {
-        body.classList.add('implaying');
-        video.play();
-      } else {
-        body.classList.remove('implaying');
-        video.pause();
-      }
-    }
-
+  $body.on('click', '.im-character', function (event) {
     // When u click a character, play the video
-    $body.on('click', '.im-character', function (event) {
-      playMedia(event);
-    });
-  }, false);
-  video.load();
+    video.play();
+    $body.addClass('implaying');
+
+    var timer = setTimeout(function () {
+      // Make sure you indicate video has ended
+      // so it can play again for all characters
+      console.log('video finished');
+      $body.removeClass('implaying');
+    }, video.duration * 1000);
+
+    // When u click character paul, mr mrjingels, melinda the class gets added = dead
+    // When u click character percy the class gets removed = well
+    $(this).toggleClass('im-character--dead');
+  });
 
   // When you hover scale that character
   $body.on('mouseenter', '.im-character', function (event) {
@@ -38,7 +27,4 @@ $(function() {
   $body.on('mouseleave', '.im-character', function (event) {
     $(this).removeClass('im-scale');
   });
-
-
-
 });
